d2b5c74e0adeac8e3095957fa84ee54fec8cd28e
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2026-05-19 12:01:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
+import React, { useState } from "react";
+import ChildComponent1 from "./ChildComponent1.jsx";
+import ChildComponent2 from "./ChildComponent2.jsx";
 
-import React from "react";
-import './../styles/App.css';
+function App() {
+  const [selectedOption, setSelectedOption] = useState("");
 
-const App = () => {
+  const handleSelection = (option) => {
+    setSelectedOption(option);
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent"
+    style={{
+      backgroundColor:"lightgreen",
+      width:"200px"
+    }}>
+      <h1>Parent Component</h1>
+
+      
+
+      <ChildComponent1 onSelect={handleSelection} />
+      <ChildComponent2 onSelect={handleSelection} />
+      <h2>
+        Selected Option:{selectedOption}
+        
+      </h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
